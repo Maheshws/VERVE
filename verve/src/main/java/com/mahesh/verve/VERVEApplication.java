c36b15c3918eb8a7be8bd3c3b3269636e52b7011
Mahesh <mahesh@mahesh.ws>
2014-01-28 20:51:22 +0530

    Added ACRA

---
+++ verve/src/main/java/com/mahesh/verve/VERVEApplication.java
@@ -0,0 +1,24 @@
+package com.mahesh.verve;
+
+/**
+ * Created by Mahesh on 1/28/14.
+ */
+import android.app.Application;
+
+import org.acra.ACRA;
+import org.acra.annotation.ReportsCrashes;
+
+
+@ReportsCrashes(
+        formKey = "", // This is required for backward compatibility but not used
+        formUri = "http://verveoverheat.in/appfiles/logs/report.php"
+)
+public class VERVEApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        // The following line triggers the initialization of ACRA
+        ACRA.init(this);
+    }
+}
